1ac060f13a14870b67ab8d2880746599c49fd9a1
Darin Boesch <darinboesch@me.com>
2017-10-15 11:48:51 -0400

    groundwork laid for redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import ReactDOM from "react-dom";
+/*eslint-disable import/default */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import { Router, browserHistory } from 'react-router';
 import routes from "./config/routes";
 
-ReactDOM.render(routes, document.getElementById("app"));
+const store = configureStore();
+//store.dispatch(loadUrls());
+
+render(routes, document.getElementById("app"));
